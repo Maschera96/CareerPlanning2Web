1c87f4b6e7d66adda065962a59610bde96614e42
马云飞 <695498150@qq.com>
2021-05-30 04:46:29 +0800

    新增查看简历功能

--- pages/index/position/position.vue
+++ pages/index/position/position.vue
@@ -15,21 +15,6 @@
 						<el-checkbox v-for="type in jobType" :label="type" :key="type">{{type}}</el-checkbox>
 					</el-checkbox-group>
 				</el-submenu>
-				<!-- <el-submenu index="3">
-					<template slot="title"><i class="el-icon-setting"></i>导航三</template>
-					<el-menu-item-group>
-						<template slot="title">分组一</template>
-						<el-menu-item index="3-1">选项1</el-menu-item>
-						<el-menu-item index="3-2">选项2</el-menu-item>
-					</el-menu-item-group>
-					<el-menu-item-group title="分组2">
-						<el-menu-item index="3-3">选项3</el-menu-item>
-					</el-menu-item-group>
-					<el-submenu index="3-4">
-						<template slot="title">选项4</template>
-						<el-menu-item index="3-4-1">选项4-1</el-menu-item>
-					</el-submenu>
-				</el-submenu> -->
 			</el-menu>
 		</el-aside>
 
@@ -38,9 +23,7 @@
 				<el-dropdown @command="newJob()">
 					<i class="el-icon-setting" style="margin-right: 15px"></i>
 					<el-dropdown-menu slot="dropdown">
-						<!-- <el-dropdown-item>查看</el-dropdown-item> -->
 						<el-dropdown-item>新增</el-dropdown-item>
-						<!-- <el-dropdown-item>删除</el-dropdown-item> -->
 					</el-dropdown-menu>
 				</el-dropdown>
 				<span>{{name}}</span>
@@ -56,10 +39,54 @@
 					</el-table-column>
 					<el-table-column prop="jobRequirement" label="岗位要求">
 					</el-table-column>
-					<el-table-column label="操作" width="150">
+					<el-table-column label="操作" width="170">
 						<template slot-scope="scope">
-							<el-popover class="mybtn" placement="top-start" title="对不起" trigger="click" content="编辑岗位功能暂未开放!">
-								<el-button size="mini" @click="editJob(scope.row)" slot="reference">编辑</el-button>
+							<el-popover class="mybtn" placement="right" width="500" trigger="click">
+								<el-table :data="resume" style="width: 100%">
+									<el-table-column type="expand">
+										<template slot-scope="props">
+											<el-form label-position="left"  label-width="100px" class="demo-table-expand">
+												<el-form-item label="姓名">
+													<span>{{ props.row.name }}</span>
+												</el-form-item>
+												<el-form-item label="生日">
+													<span>{{ props.row.birthday }}</span>
+												</el-form-item>
+												<el-form-item label="邮箱">
+													<span>{{ props.row.email }}</span>
+												</el-form-item>
+												<el-form-item label="期望岗位">
+													<span>{{ props.row.expectedJob }}</span>
+												</el-form-item>
+												<el-form-item label="期望工作地点">
+													<span>{{ props.row.expectedPlace }}</span>
+												</el-form-item>
+												<el-form-item label="电话号码">
+													<span>{{ props.row.telephone }}</span>
+												</el-form-item>
+												<el-form-item label="最高学历">
+													<span>{{ props.row.highestEducation }}</span>
+												</el-form-item>
+												<el-form-item label="自我描述">
+													<span>{{ props.row.selfDescription }}</span>
+												</el-form-item>
+											</el-form>
+										</template>
+									</el-table-column>
+									<el-table-column label="最高学历" prop="highestEducation">
+									</el-table-column>
+									<el-table-column label="姓名" prop="name">
+									</el-table-column>
+									<el-table-column label="学校" prop="educationExperience[0].university">
+									</el-table-column>
+								</el-table>
+								<!-- <el-table :data="resume">
+									<el-table-column width="100" property="highestEducation" label="最高学历"></el-table-column>
+									<el-table-column width="100" property="name" label="姓名"></el-table-column>
+									<el-table-column width="100" property="expectedJob" label="期望工作"></el-table-column>
+									<el-table-column width="300" property="educationExperience[0].university" label="学校"></el-table-column>
+								</el-table> -->
+								<el-button size="mini" slot="reference" @click="getResume(scope.row)">查看简历</el-button>
 							</el-popover>
 							<el-popconfirm confirm-button-text='删除' cancel-button-text='不用了' icon="el-icon-info"
 								icon-color="red" title="确定删除该岗位嘛？" @confirm="deleteJob(scope.row)">
@@ -77,13 +104,25 @@
 <script>
 	export default {
 		data() {
-			const item = {
-				date: '2016-05-02',
-				name: '王小虎',
-				address: '上海市普陀区金沙江路 1518 弄'
-			};
 			return {
-				tableData: Array(20).fill(item),
+				gridData: [{
+					date: '2016-05-02',
+					name: '王小虎',
+					address: '上海市普陀区金沙江路 1518 弄'
+				}, {
+					date: '2016-05-04',
+					name: '王小虎',
+					address: '上海市普陀区金沙江路 1518 弄'
+				}, {
+					date: '2016-05-01',
+					name: '王小虎',
+					address: '上海市普陀区金沙江路 1518 弄'
+				}, {
+					date: '2016-05-03',
+					name: '王小虎',
+					address: '上海市普陀区金沙江路 1518 弄'
+				}],
+				resume: [],
 				job: [],
 				name: '',
 				checkedCities: ['上海', '北京'],
@@ -123,8 +162,16 @@
 			handleCheckedCitiesChange(value) {
 				let [pageIndex, pageSize] = [1, 10]
 				let _jobPlace, _jobType
-				if(this.checkedCities.length === 0){_jobPlace = this.cities.join(' ')}else{_jobPlace = this.checkedCities.join(' ')}
-				if(this.checkedType.length === 0){_jobType = this.jobType.join(' ')}else{_jobType = this.checkedType.join(' ')}
+				if (this.checkedCities.length === 0) {
+					_jobPlace = this.cities.join(' ')
+				} else {
+					_jobPlace = this.checkedCities.join(' ')
+				}
+				if (this.checkedType.length === 0) {
+					_jobType = this.jobType.join(' ')
+				} else {
+					_jobType = this.checkedType.join(' ')
+				}
 				uni.request({
 					url: `http://1.15.175.248:8004/search/job/${pageIndex}/${pageSize}`,
 					method: 'POST',
@@ -144,6 +191,17 @@
 			},
 			editJob(item) {
 				console.log('edit', item.jobName);
+			},
+			getResume(item) {
+				// console.log(item);
+				let jobId = item.indexCode
+				uni.request({
+					url: `http://1.15.175.248:8002/job/${item.indexCode}/get/resume/1/10`,
+					success: (res) => {
+						console.log(res.data.data.data);
+						this.resume = res.data.data.data
+					}
+				})
 			},
 			deleteJob(item) {
 				console.log('delete', item.jobName);
@@ -176,7 +234,7 @@
 		color: #333;
 	}
 
-	.mybtn{
+	.mybtn {
 		margin-right: 10px;
 	}
 
@@ -188,5 +246,23 @@
 
 	.el-checkbox {
 		margin: 10px 0px 10px 40px;
+	}
+
+	.demo-table-expand {
+		font-size: 0;
+	}
+
+	/* .demo-table-expand label */
+
+	.el-form-item label {
+		width: 90px !important;
+		color: #99a9bf !important;
+	}
+
+	.el-form-item {
+		margin-right: 0;
+		margin-bottom: 0;
+		/* width: 50%; */
+		/* color: #99a9bf; */
 	}
 </style>
